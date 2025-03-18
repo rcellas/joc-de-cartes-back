3988ddf6bfedd946f5d80b53f52c5e4f4d1dfd06
rcellas <rocio.cejudo.soronellas@gmail.com>
2025-03-18 23:49:32 +0100

    fix the test for ProgramasController

--- tests/Feature/Http/Controllers/ProgramControllerTest.php
+++ tests/Feature/Http/Controllers/ProgramControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Program;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -14,7 +15,7 @@ class ProgramControllerTest extends TestCase
     public function testIndex()
     {
         $program = Program::factory()->count(3)->create();
-        $response = $this->getJson('/programs');
+        $response = $this->getJson('/api/programs');
         $response->assertStatus(200)
                  ->assertJsonCount(3);
     }
@@ -22,20 +23,26 @@ class ProgramControllerTest extends TestCase
     public function testShow()
     {
         $program = Program::factory()->create();
-        $response = $this->getJson("/programs/{$program->id}");
+        $response = $this->getJson("/api/programs/{$program->id}");
         $response->assertStatus(200)
                  ->assertJsonFragment(['name' => $program->name]);
     }
 
     public function testShowNotFound()
     {
-        $response = $this->getJson('/programs/999999');
+        $admin = User::factory()->createOne(['role' => 'admin']);
+        $this->actingAs($admin);
+
+        $response = $this->getJson('/api/programs/999999');
         $response->assertStatus(404);
         $response->assertJsonFragment(['message' => 'El programa no exiteix a les bases de dades']);
     }
 
     public function testStore()
     {
+        $admin = User::factory()->createOne(['role' => 'admin']);
+        $this->actingAs($admin);
+
         $data=[
             'name'=>'Test Program',
             'description'=>'Test Description',
@@ -43,7 +50,7 @@ class ProgramControllerTest extends TestCase
             'season'=>4,
         ];
 
-        $response = $this->postJson('/programs', $data);
+        $response = $this->postJson('/api/programs', $data);
         $response->assertStatus(201);
         $response->assertJsonFragment(['name' => 'Test Program']);
     }
@@ -56,39 +63,51 @@ class ProgramControllerTest extends TestCase
             'season' => 4,
         ];
 
-        $response = $this->postJson('/programs', $data);
+        $response = $this->postJson('/api/programs', $data);
         $response->assertStatus(422);
         $response->assertJsonValidationErrors('year');
     }
 
     public function testUpdate()
     {
+        $admin = User::factory()->createOne(['role' => 'admin']);
+        $this->actingAs($admin);
+
         $program = Program::factory()->create();
         $data = ['name' => 'New Name'];
-        $response = $this->putJson("/programs/{$program->id}", $data);
+        $response = $this->putJson("/api/programs/{$program->id}", $data);
         $response->assertStatus(200)
                  ->assertJsonFragment(['name' => 'New Name']);
     }
 
     public function testUpdateNotFound()
     {
+        $admin = User::factory()->createOne(['role' => 'admin']);
+        $this->actingAs($admin);
+
         $data = ['name' => 'New Name'];
-        $response = $this->putJson('/programs/999999', $data);
+        $response = $this->putJson('/api/programs/999999', $data);
         $response->assertStatus(404);
         $response->assertJsonFragment(['message' => 'El programa no exiteix a les bases de dades']);
     }
 
     public function testDestroy()
     {
+        $admin = User::factory()->createOne(['role' => 'admin']);
+        $this->actingAs($admin);
+
         $program = Program::factory()->create();
-        $response = $this->deleteJson("/programs/{$program->id}");
+        $response = $this->deleteJson("/api/programs/{$program->id}");
         $response->assertStatus(200)
                  ->assertJsonFragment(['id' => $program->id]);
     }
 
     public function testDestroyNotFound()
     {
-        $response = $this->deleteJson('/programs/999999');
+        $admin = User::factory()->createOne(['role' => 'admin']);
+        $this->actingAs($admin);
+
+        $response = $this->deleteJson('/api/programs/999999');
         $response->assertStatus(404);
         $response->assertJsonFragment(['message' => 'El programa no exiteix a les bases de dades']);
     }
